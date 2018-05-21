fe76158cb4d2a27d92809c861f56fb5729454a98
Nádia Carvalho <nadiacarvalho118@gmail.com>
2018-05-21 18:07:43 +0100

    commit search system
    commit search system

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -8,13 +8,21 @@ use App\User;
 
 class SearchController extends Controller
 {
-  public function searchSystem(Request $request)
-  {
-    $search = $request->input('search');
-    return view('pages.searchResults',['search' => $search,'results' => $this->search($search)]);
-  }
+    public function searchSystem(Request $request)
+    {
+        $search = $request->input('search');
 
-  public function search($search){
-    return User::whereRaw('textsearchable_user_col @@ plainto_tsquery(\'english\', ?)',[$search])->orderByRaw('ts_rank(textsearchable_user_col, plainto_tsquery(\'english\', ?)) DESC', [$search])->get()->merge(Auction::whereRaw('textsearchable_auction_col @@ plainto_tsquery(\'english\', ?)',[$search])->orderByRaw('ts_rank(textsearchable_auction_col, plainto_tsquery(\'english\', ?)) DESC', [$search])->get());
-  }
+        return view('pages.searchResults', ['search' => $search, 'results' => $this->search($search)]);
+    }
+
+    public function search($search)
+    {
+        $results_auctions = Auction::whereRaw('textsearchable_auction_col @@ plainto_tsquery(\'english\', ?)', [$search])->orderByRaw('ts_rank(textsearchable_auction_col, plainto_tsquery(\'english\', ?)) DESC', [$search])->paginate(5,['*'], '_auctions');
+        $results_users = User::whereRaw('textsearchable_user_col @@ plainto_tsquery(\'english\', ?)',[$search])->orderByRaw('ts_rank(textsearchable_user_col, plainto_tsquery(\'english\', ?)) DESC', [$search])->paginate(5,['*'], '_users');
+
+        $results_auctions->withPath('search?search='.$search);
+        $results_users->withPath('search?search='.$search);
+
+        return ['users' => $results_users, 'auctions' => $results_auctions];
+    }
 }
--- resources/views/partials/searchResults.blade.php
+++ resources/views/partials/searchResults.blade.php
@@ -7,6 +7,7 @@
 </div>
 
 <div class="container w-65">
+  <h4>{{ $search }} in Auctions</h4>
   <table class="table table-hover">
     <thead>
       <tr class="table">
@@ -16,8 +17,7 @@
       </tr>
     </thead>
     <tbody>
-      @foreach($results as $result)
-      @if ($result instanceof App\Auction)
+      @foreach($results['auctions'] as $result)
       <tr>
         <td>{{ $result->title }}</td>
         <td>Auction</td>
@@ -29,54 +29,39 @@
           </a>
         </td>
       </tr>
-      @elseif ($result instanceof App\User)
-      <tr>
-        <td>{{ $result->username }}</td>
-        <td>User</td>
-        <td>
-          <a href="/user/{{ $result->id }}">
-            <img src="images/goto_link.png"
-            width="20"
-            height="20" />
-          </a>
-        </td>
-      </tr>
-      @endif
       @endforeach
     </tbody>
   </table>
   <br />
-  <div id="pagination">
-    <ul class="pagination pagination-sm">
-      <li class="page-item disabled">
-        <a class="page-link"
-           href="#">&laquo;</a>
-      </li>
-      <li class="page-item active">
-        <a class="page-link"
-           href="#">1</a>
-      </li>
-      <li class="page-item">
-        <a class="page-link"
-           href="#">2</a>
-      </li>
-      <li class="page-item">
-        <a class="page-link"
-           href="#">3</a>
-      </li>
-      <li class="page-item">
-        <a class="page-link"
-           href="#">4</a>
-      </li>
-      <li class="page-item">
-        <a class="page-link"
-           href="#">5</a>
-      </li>
-      <li class="page-item">
-        <a class="page-link"
-           href="#">&raquo;</a>
-      </li>
-    </ul>
-  </div>
+  {{ $results['auctions']->fragment('_auctions')->links() }}
 </div>
 <br />
+<div class="container w-65">
+<h4>{{ $search }} in Users</h4>
+<table class="table table-hover">
+  <thead>
+    <tr class="table">
+      <th>Item</th>
+      <th>Context</th>
+      <th>Go to Page</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($results['users'] as $result)
+    <tr>
+      <td>{{ $result->username }}</td>
+      <td>User</td>
+      <td>
+        <a href="/user/{{ $result->id }}">
+          <img src="images/goto_link.png"
+          width="20"
+          height="20" />
+        </a>
+      </td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
+<br />
+{{ $results['users']->fragment('_users')->links() }}
+</div>
