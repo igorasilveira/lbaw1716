de910897b84e11a10cf9bf3ad129d407533a7210
Nádia Carvalho <nadiacarvalho118@gmail.com>
2018-05-29 00:25:28 +0100

    controller

--- routes/web.php
+++ routes/web.php
@@ -60,6 +60,8 @@ Route::get('/admin/auction/{id}/approve', 'AdminController@approveAuction');
 Route::get('/admin/auction/{id}/reject', 'AdminController@rejectAuction');
 Route::delete('/admin/manage/moderators/{username}/remove', 'AdminController@deleteModerator');
 Route::delete('/admin/manage/categories/{id}/remove', 'AdminController@deleteCategory');
+Route::post('/admin/manage/moderators/add', 'AdminController@addModerator');
+Route::post('/admin/manage/categories/add', 'AdminController@addCategory');
 
 /*
 Route::get('/admin/auctions', '')->name('');
@@ -69,8 +71,6 @@ Route::get('/admin/users/{id}', '')->name('');
 Route::put('/admin/users/{id}/block', '');
 Route::put('/admin/users/{id}/unblock', '');
 Route::get('/admin/manage', '')->name('');
-Route::post('/admin/manage/moderators/add', '');
-Route::post('/admin/manage/categories/add', '');
 */
 
 // Module 4:
