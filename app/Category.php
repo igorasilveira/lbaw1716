b345c37e26b4de064717735e34b44b8a05cc22d6
Nádia Carvalho <nadiacarvalho118@gmail.com>
2018-05-29 01:49:42 +0100

    paginação nas categorias
    paginação nas categorias

--- app/Category.php
+++ app/Category.php
@@ -6,29 +6,39 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-  /**
-  * The table associated with the model.
-  *
-  * @var string
-  */
-  protected $table = 'category';
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'category';
 
-  protected $fillable = ['name', 'parent'];
+    protected $fillable = ['name', 'parent'];
 
-  public $timestamps = false;
+    public $timestamps = false;
 
-  public function parent()
-  {
-    return self::find(self::parent);
-  }
+    public function parent()
+    {
+        return self::find(self::parent);
+    }
 
-  public function auctions()
-  {
-    return $this->belongsToMany('App\Auction','categoryofauction');
-  }
+    public function auctions()
+    {
+        return $this->belongsToMany('App\Auction', 'categoryofauction');
+    }
 
-  public function hasChilds()
-  {
-    return static::where('parent', $this['id'])->count();
-  }
+    public function active_auctions()
+    {
+        return $this->auctions()->where('state', 'Active')->orderby('numberofbids', 'DESC');
+    }
+
+    public function active_auctions_m6()
+    {
+        return $this->auctions()->where('state', 'Active')->orderby('numberofbids', 'DESC')->skip(6)->paginate(5);
+    }
+
+    public function hasChilds()
+    {
+        return static::where('parent', $this['id'])->count();
+    }
 }
--- resources/views/partials/category.blade.php
+++ resources/views/partials/category.blade.php
@@ -13,7 +13,7 @@
       <ul id="auctionsMosaic"
           class="container-fluid col-sm-12 row">
         <div class="row">
-          @foreach($category->auctions()->where('state','Active')->orderby('numberofbids', 'DESC')->take(3)->get() as $auction)
+          @foreach($category->active_auctions()->take(3)->get() as $auction)
           @if($category->auctions()->where('state','Active')->count()>=3)
           <li class="col-md-4 col-sm-6 col-xs-6">
           @elseif($category->auctions()->where('state','Active')->count()==2)
@@ -28,7 +28,7 @@
         <hr class="my-5">
         <div class="row">
           @if($category->auctions->count() >= 6)
-          @foreach($category->auctions()->where('state','Active')->orderby('numberofbids', 'DESC')->skip(3)->take(3)->get() as $auction)
+          @foreach($category->active_auctions()->skip(3)->take(3)->get() as $auction)
           @if($category->auctions()->where('state','Active')->count()>=6)
           <li class="col-md-4 col-sm-6 col-xs-6">
           @elseif($category->auctions()->where('state','Active')->count()==5)
@@ -44,26 +44,28 @@
       </ul>
       <br />
     </div>
+
+    @if($category->auctions->count() >= 6)
     <button type="button"
-            class="btn
-                  btn-block
-                  btn-secondary
-                  dropdown-toggle"
-            data-toggle="collapse"
-            data-target=".bidsListViewMore">
-            View More
-  </button>
+    class="btn
+    btn-block
+    btn-secondary
+    dropdown-toggle"
+    data-toggle="collapse"
+    data-target=".bidsListViewMore">
+    View More
+    </button>
     <div>
       <div class="bidsListViewMore container-fluid collapse">
         <div class="container-fluid">
           <hr class="my-3">
           <form class="form-inline my-2 my-lg-0">
             <input class="form-control col-7"
-                   placeholder="Search"
-                   type="text">
+            placeholder="Search"
+            type="text">
             <span class="col-1"></span>
             <button class="btn btn-primary my-2 col-4"
-                    type="submit">Search</button>
+            type="submit">Search</button>
           </form>
           <hr class="my-3">
         </div>
@@ -73,85 +75,33 @@
               <th scope="col">Photo of Item</th>
               <th scope="col">Name of Auction</th>
               <th scope="col">Value of last Bid</th>
-              <th scope="col">Lifetime of Auction</th>
+              <th scope="col">Open Untill</th>
             </tr>
           </thead>
           <tbody>
+            @foreach($category->active_auctions_m6() as $auction)
             <tr>
-              <td scope="row"><img src="https://static.fnac-static.com/multimedia/Images/PT/NR/57/8a/15/1411671/1540-1.jpg"
-                     width="80"
-                     height="80"></td>
-              <td> <a href="#"> Microsoft Surface Book 2 - 13'' - i7-8650U | 8GB | 256GB </a> </td>
-              <td> 1500€ </td>
-              <td> 1-03-2018 </td>
-            </tr>
-            <tr>
-              <td scope="row"><img src="https://static.fnac-static.com/multimedia/Images/PT/NR/b7/78/14/1341623/1539-1.jpg"
-                     width="80"
-                     height="80"></td>
-              <td> <a href="#"> Laptop Acer Swift 5 SF514-52T | Core i5-8250U | 256GB </a> </td>
-              <td> 1000€ </td>
-              <td> 10-03-2018 </td>
-            </tr>
-            <tr>
-              <td scope="row"><img src="https://static.fnac-static.com/multimedia/Images/PT/NR/b7/16/12/1185463/1539-1/tsp20170614164417/Apple-MacBook-Air-13-i5-1-8GHz-8GB-128GB.jpg"
-                     width="80"
-                     height="80"></td>
-              <td> <a href="#"> Apple MacBook Air 13'' i5-1,8GHz | 8GB | 128GB </a> </td>
-              <td> 500€ </td>
-              <td> 5-03-2018 </td>
-            </tr>
-            <tr>
-              <td scope="row"><img src="https://static.fnac-static.com/multimedia/Images/PT/NR/73/6e/14/1338995/1540-1.jpg"
-                     width="80"
-                     height="80"></td>
-              <td> <a href="#"> Laptop Asus Zenbook Flip S UX370UA-78DHDAB1 </a> </td>
-              <td> 100€ </td>
-              <td> 1-03-2018 </td>
-            </tr>
-            <tr>
-              <td scope="row"><img src="https://static.fnac-static.com/multimedia/Images/PT/NR/5c/76/15/1406556/1540-1.jpg"
-                     width="80"
-                     height="80"></td>
-              <td> <a href="#"> HP Spectre 13-af002np Laptop </a> </td>
-              <td> 400€ </td>
-              <td> 1-03-2018 </td>
-            </tr>
-          </tbody>
-        </table>
-        <br />
-        <div id="pagination">
-          <ul class="pagination pagination-sm">
-            <li class="page-item disabled">
-              <a class="page-link"
-                 href="#">&laquo;</a>
-            </li>
-            <li class="page-item active">
-              <a class="page-link"
-                 href="#">1</a>
-            </li>
-            <li class="page-item">
-              <a class="page-link"
-                 href="#">2</a>
-            </li>
-            <li class="page-item">
-              <a class="page-link"
-                 href="#">3</a>
-            </li>
-            <li class="page-item">
-              <a class="page-link"
-                 href="#">4</a>
-            </li>
-            <li class="page-item">
-              <a class="page-link"
-                 href="#">5</a>
-            </li>
-            <li class="page-item">
-              <a class="page-link"
-                 href="#">&raquo;</a>
-            </li>
-          </ul>
+              <td scope="row"><img src="{{ $auction->pathtophoto }}"
+                width="80"
+                height="80"></td>
+                <td width="50%"> <a href="/auction/{{ $auction->id }}"> {{ $auction->title }} </a> </td>
+                <td>
+                  @if($auction->bids->count() > 0)
+                  {{ $auction->bids->sortByDesc('date')->first()->value }} €
+                  @else
+                  0 €
+                  @endif
+                </td>
+                <td> {{ $auction->timeleft() }} </td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+          <br />
+          <div class="container-fluid my-4">
+            {{ $category->active_auctions_m6()->links() }}
+          </div>
         </div>
       </div>
-    </div>
+    @endif
   </div>
