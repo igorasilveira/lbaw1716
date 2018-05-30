caba18db39199a8090f6e40b0664029eab397b43
Nádia Carvalho <nadiacarvalho118@gmail.com>
2018-05-30 07:10:05 +0100

    a ver notificações
     a ver notificações

--- app/Notification.php
+++ app/Notification.php
@@ -15,18 +15,41 @@ class Notification extends Model
   */
   protected $table = 'notification';
 
-
   protected $fillable = [
-    'date', 'description', 'type', 'auctionassociated', 'authenticated_userid', '_read'
+    'date', 'description', 'type', 'auctionassociated', 'authenticated_userid', '_read',
   ];
+
+
+  function descriptionCont() {
+
+    switch($this->type){
+      case 'Auction Accepted':
+      $this->description = "Your Auction <a href='/auction/" . $this->auctionassociated . "'> #" . $this->auctionassociated . "</a> was accepted!";
+      break;
+      case 'Auction Rejected':
+      break;
+      case 'Auction Created':
+      break;
+      case 'Auction Over':
+      break;
+      case 'Bid Exceeded':
+      break;
+      case 'Won Auction':
+      break;
+      default:
+      break;
+    }
+    $this->save();
+
+  }
 
   public function user()
   {
-    return $this->belongsTo('App\User','authenticated_userid');
+    return $this->belongsTo('App\User', 'authenticated_userid');
   }
 
   public function auction()
   {
-    return $this->belongsTo('App\Auction','auctionassociated');
+    return $this->belongsTo('App\Auction', 'auctionassociated');
   }
 }
